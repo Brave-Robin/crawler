382a384cc117729fc2e2c233dbdfaf4dd4fa3c74
Irakli Shaverdashvili <ikaa.shaverdashvili@gmail.com>
2023-08-17 14:54:46 +0400

    add get_image

--- ebay.py
+++ ebay.py
@@ -4,6 +4,7 @@
 import requests
 import yaml
 import bs4
+import re
 import sqlite3
 # import os.path
 
@@ -32,48 +33,63 @@ def get_content(url, headers):
     return requests.get(url, headers=headers).content
 
 
-def get_info(url, headers, parser_type, title, price, availability, db_storage: bool = False):
-    """
-    :param db_storage:
-    :param url:
-    :param headers:
-    :param parser_type:
-    :param title:
-    :param price:
-    :param availability:
-    :return:
-    """
-    item_dic = {}
-    if db_storage:
-        con = sqlite3.connect("ebay.sqlite")
-        cur = con.cursor()
-    try:
-        item_dic['title'] = bs4.BeautifulSoup(get_content(url, headers), config['ParserType']).select_one(
-            config['TITLE']).string.strip()
-    except AttributeError:
-        item_dic['title'] = "No data"
-    try:
-        item_dic['price'] = bs4.BeautifulSoup(get_content(url, headers), config['ParserType']).select_one(
-            config['PRICE']).string.strip()
-    except AttributeError:
-        item_dic['price'] = "No data"
-    try:
-        item_dic['availability'] = bs4.BeautifulSoup(get_content(url, headers), config['ParserType']).select_one(
-            config['AVAILABILITY']).string.strip()
-    except AttributeError:
-        item_dic['availability'] = "No data"
-    if db_storage:
-        if cur.execute("SELECT 1 FROM storage WHERE url = ?", [url]).fetchone():
-            if not cur.execute("SELECT 1 FROM storage WHERE title = ? AND price = ? AND availability = ?", (item_dic['title'], item_dic['price'], item_dic['availability'])).fetchone():
-                print("Need update")
-                cur.execute("UPDATE storage SET title = ?, price = ?, availability = ? WHERE url = ?",
-                            (item_dic['title'], item_dic['price'], item_dic['availability'], url))
-        else:
-            cur.execute("INSERT INTO storage (url, title, price, availability) VALUES (?, ?, ?, ?)", (url, item_dic['title'], item_dic['price'], item_dic['availability']))
-            con.commit()
-    return item_dic
+def get_pages(url, headers, parser_type):
+    total_items =  bs4.BeautifulSoup(get_content(url, headers), config['ParserType']).select_one('.srp-controls__count-heading > span:nth-child(1)').string.strip()
+    item_per_page = bs4.BeautifulSoup(get_content(url, headers), config['ParserType']).select_one('#srp-ipp-menu > button:nth-child(1) > span:nth-child(1) > span:nth-child(1)').string.strip()
+    total_items = total_items.replace(",", "")
+    total_pages = int(total_items) / int(item_per_page)
+    ### LIMIT is 42
+    if total_pages > 42:
+        total_pages = 42
+    page_list = []
+    for page in range(1, total_pages + 1):
+        each_url = config['URL']+"&_pgn=" + str(page)
+        page_list.append(each_url)
+    return page_list
 
+print(get_pages(config['URL'], HEADERS, config['ParserType']))
 
+# def get_info(url, headers, parser_type, title, price, availability, db_storage: bool = False):
+#     """
+#     :param db_storage:
+#     :param url:
+#     :param headers:
+#     :param parser_type:
+#     :param title:
+#     :param price:
+#     :param availability:
+#     :return:
+#     """
+#     item_dic = {}
+#     if db_storage:
+#         con = sqlite3.connect("ebay.sqlite")
+#         cur = con.cursor()
+#     try:
+#         item_dic['title'] = bs4.BeautifulSoup(get_content(url, headers), config['ParserType']).select_one(
+#             config['TITLE']).string.strip()
+#     except AttributeError:
+#         item_dic['title'] = "No data"
+#     try:
+#         item_dic['price'] = bs4.BeautifulSoup(get_content(url, headers), config['ParserType']).select_one(
+#             config['PRICE']).string.strip()
+#     except AttributeError:
+#         item_dic['price'] = "No data"
+#     try:
+#         item_dic['availability'] = bs4.BeautifulSoup(get_content(url, headers), config['ParserType']).select_one(
+#             config['AVAILABILITY']).string.strip()
+#     except AttributeError:
+#         item_dic['availability'] = "No data"
+#     if db_storage:
+#         if cur.execute("SELECT 1 FROM storage WHERE url = ?", [url]).fetchone():
+#             if not cur.execute("SELECT 1 FROM storage WHERE title = ? AND price = ? AND availability = ?", (item_dic['title'], item_dic['price'], item_dic['availability'])).fetchone():
+#                 print("Need update")
+#                 cur.execute("UPDATE storage SET title = ?, price = ?, availability = ? WHERE url = ?",
+#                             (item_dic['title'], item_dic['price'], item_dic['availability'], url))
+#         else:
+#             cur.execute("INSERT INTO storage (url, title, price, availability) VALUES (?, ?, ?, ?)", (url, item_dic['title'], item_dic['price'], item_dic['availability']))
+#             con.commit()
+#     return item_dic
+#
 def get_links(url, headers, parser_type):
     """
     :param url:
@@ -94,31 +110,32 @@ def get_links(url, headers, parser_type):
                 links_list.append(link)
     # TODO Need to fix doubles
     # TODO add pagination
+    del links_list[::2]
     return links_list
-
-
-def get_each_items_data(url, headers, parser_type, title, price, availability, db_storage: bool = False):
-    """
-    :param db_storage:
-    :param url:
-    :param headers:
-    :param parser_type:
-    :param title:
-    :param price:
-    :param availability:
-    :return:
-    """
-    result_list_data = []
-    for link in get_links(url, headers, parser_type):
-        if db_storage:
-            get_info(link, headers, parser_type, title, price, availability, db_storage)
-        else:
-            result_list_data.append(get_info(link, headers, parser_type, title, price, availability))
-    return result_list_data
-
-
-all_elements_from_page = get_each_items_data(config['URL'], HEADERS, config['ParserType'], config['TITLE'],
-                                             config['PRICE'], config['AVAILABILITY'], True)
+#
+#
+# def get_each_items_data(url, headers, parser_type, title, price, availability, db_storage: bool = False):
+#     """
+#     :param db_storage:
+#     :param url:
+#     :param headers:
+#     :param parser_type:
+#     :param title:
+#     :param price:
+#     :param availability:
+#     :return:
+#     """
+#     result_list_data = []
+#     for link in get_links(url, headers, parser_type):
+#         if db_storage:
+#             get_info(link, headers, parser_type, title, price, availability, db_storage)
+#         else:
+#             result_list_data.append(get_info(link, headers, parser_type, title, price, availability))
+#     return result_list_data
+#
+#
+# all_elements_from_page = get_each_items_data(config['URL'], HEADERS, config['ParserType'], config['TITLE'],
+#                                              config['PRICE'], config['AVAILABILITY'], True)
 
 # print(all_elements_from_page)
 # print(len(all_elements_from_page))
